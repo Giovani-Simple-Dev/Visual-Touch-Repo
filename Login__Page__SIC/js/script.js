d4bc1268e1cf4d73972fb1ddc9fc1a86b850bd4f
Giovani-Simple-Dev <167205762+Giovani-Simple-Dev@users.noreply.github.com>
2024-09-20 20:56:17 -0300

    upload

---
+++ Login__Page__SIC/js/script.js
@@ -0,0 +1,17 @@
+function register() {
+    document.getElementById('loginForm').style.display = 'none';
+    document.getElementById('registerForm').style.display = 'flex';
+}
+
+function login() {
+    window.location.href = 'index.html';
+}
+
+function cadastro() {
+    alert('⚠️​Função em Desenvolvimento⚠️​')
+}
+
+function loginF() {
+    document.getElementById('registerForm').style.display = 'none';
+    document.getElementById('loginForm').style.display = 'flex';
+}
