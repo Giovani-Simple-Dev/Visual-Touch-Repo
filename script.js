4170ebe20a0378b781bcec398fd0edc71a35c131
Giovani-Simple-Dev <167205762+Giovani-Simple-Dev@users.noreply.github.com>
2024-10-17 09:48:56 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('activeBtn').addEventListener('click', function(){
-    const button = document.getElementById('activeBtn');
-
-    if (button.innerText == 'Ativar Serviço') {
-        button.innerText = 'Desativar Serviço';
-    } else {
-        button.innerText = 'Ativar Serviço';
-    }
-});
